339329ec17bce26b07d62ed6202dba8388fed6ff
mitsuru <codeberg@yamamech.sh>
2024-07-07 01:18:02 +0100

    Moved Perfect Dark packages to be separate per-region

--- default.nix
+++ default.nix
@@ -6,11 +6,13 @@
   # overlays = import ./overlays; # nixpkgs overlays
 
   macos-cursors = pkgs.callPackage ./pkgs/macOS-cursors { };
-  perfect-dark = pkgs.callPackage ./pkgs/perfect-dark { };
   tokyo-night-gtk-icons = pkgs.callPackage ./pkgs/tokyo-night-gtk-icons { };
   tokyo-night-sddm = pkgs.callPackage ./pkgs/tokyo-night-sddm { };
 
   suyu = (pkgs.callPackage ./pkgs/suyu { }).suyu;
   citra-canary = (pkgs.callPackage ./pkgs/citra { }).canary;
   citra-nightly = (pkgs.callPackage ./pkgs/citra { }).nightly;
+  perfect-dark-ntsc = (pkgs.callPackage ./pkgs/perfect-dark { }).ntsc;
+  perfect-dark-pal = (pkgs.callPackage ./pkgs/perfect-dark { }).pal;
+  perfect-dark-jpn = (pkgs.callPackage ./pkgs/perfect-dark { }).jpn;
 }
--- pkgs/perfect-dark/default.nix
+++ pkgs/perfect-dark/default.nix
@@ -1,83 +1,10 @@
-{ lib, stdenv, pkgs, fetchurl, autoPatchelfHook, makeDesktopItem, ... }:
+{ pkgs, makeScopeWithSplicing', generateSplicesForMkScope }:
 
-let
-  iconBaseUrl = "https://cdn2.steamgriddb.com/icon/64314c17210c549a854f1f1c7adce8b6/32/";
-
-  icon256 = fetchurl {
-    url = "${iconBaseUrl}256x256.png";
-    hash = "sha256-ER5OCFENyrd7R4YCCHI/UldpUw6/zQjH7QniJOnEH/E=";
-  };
-
-  ntscDesktopItem = makeDesktopItem {
-    name = "pd";
-    desktopName = "Perfect Dark (NTSC)";
-    comment = "work in progress port of n64decomp/perfect_dark to modern platforms";
-    genericName = "First-person shooter";
-    categories = [ "Game" ];
-    exec = "pd";
-    icon = "pd";
-  };
-
-  palDesktopItem = makeDesktopItem {
-    name = "pd.pal";
-    desktopName = "Perfect Dark (PAL)";
-    comment = "work in progress port of n64decomp/perfect_dark to modern platforms";
-    genericName = "First-person shooter";
-    categories = [ "Game" ];
-    exec = "pd.pal";
-    icon = "pd";
-  };
-
-  jpnDesktopItem = makeDesktopItem {
-    name = "pd.jpn";
-    desktopName = "Perfect Dark (JPN)";
-    comment = "work in progress port of n64decomp/perfect_dark to modern platforms";
-    genericName = "First-person shooter";
-    categories = [ "Game" ];
-    exec = "pd.jpn";
-    icon = "pd";
-  };
-in 
-pkgs.pkgsi686Linux.stdenv.mkDerivation rec {
-  pname = "perfect-dark";
-  version = "latest";
-
-  src = fetchurl {
-    url = "https://github.com/fgsfdsfgs/perfect_dark/releases/download/ci-dev-build/pd-i686-linux.tar.gz";
-    hash = "sha256-7nkWfcXOsWt2qO4QkYMiqaPIULimhOgqoWsvsLvSrAo=";
-  };
-
-  buildInputs = with pkgs.pkgsi686Linux; [
-    SDL2
-  ];
-
-  nativeBuildInputs = with pkgs.pkgsi686Linux; [
-    autoPatchelfHook
-  ];
-
-  installPhase = ''
-    runHook preInstall
-    mkdir -p $out/{bin,share/{applications/,icons/hicolor/scalable/apps/}}
-    install -m755 -D pd $out/bin/pd
-    install -m755 -D pd.pal $out/bin/pd.pal
-    install -m755 -D pd.jpn $out/bin/pd.jpn
-    cp "${ntscDesktopItem}/share/applications/pd.desktop" $out/share/applications/pd.desktop
-    cp "${palDesktopItem}/share/applications/pd.pal.desktop" $out/share/applications/pd.pal.desktop
-    cp "${jpnDesktopItem}/share/applications/pd.jpn.desktop" $out/share/applications/pd.jpn.desktop
-    cp ${icon256} $out/share/icons/hicolor/scalable/apps/pd.png
-    runHook postInstall
-  '';
-
-  desktopItems = [
-    ntscDesktopItem
-    palDesktopItem
-    jpnDesktopItem
-  ];
-
-  meta = with lib; {
-    description = "work in progress port of n64decomp/perfect_dark to modern platforms";
-    homepage = "https://github.com/fgsfdsfgs/perfect_dark";
-    license = licenses.mit;
-    platforms = platforms.linux;
+makeScopeWithSplicing' {
+  otherSplices = generateSplicesForMkScope "perfectDarkPackages";
+  f = self: pkgs // {
+    ntsc = self.callPackage ./generic.nix { region = "ntsc"; };
+    pal = self.callPackage ./generic.nix { region = "pal"; };
+    jpn = self.callPackage ./generic.nix { region = "jpn"; };
   };
 }
