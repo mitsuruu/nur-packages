499ab7eac6d8c956a8ccbef8d55ff172c5410bd8
mitsuru <codeberg@yamamech.sh>
2024-07-19 23:08:31 +0100

    Added flight-core package

--- default.nix
+++ default.nix
@@ -4,15 +4,15 @@
   # lib = import ./lib { inherit pkgs; }; # functions
   # modules = import ./modules; # NixOS modules
   # overlays = import ./overlays; # nixpkgs overlays
-
+  flight-core = pkgs.callPackage ./pkgs/flight-core { };
   macos-cursors = pkgs.callPackage ./pkgs/macOS-cursors { };
   tokyo-night-gtk-icons = pkgs.callPackage ./pkgs/tokyo-night-gtk-icons { };
   tokyo-night-sddm = pkgs.callPackage ./pkgs/tokyo-night-sddm { };
 
-  suyu = (pkgs.callPackage ./pkgs/suyu { }).suyu;
   citra-canary = (pkgs.callPackage ./pkgs/citra { }).canary;
   citra-nightly = (pkgs.callPackage ./pkgs/citra { }).nightly;
+  perfect-dark-jpn = (pkgs.callPackage ./pkgs/perfect-dark { }).jpn;
   perfect-dark-ntsc = (pkgs.callPackage ./pkgs/perfect-dark { }).ntsc;
   perfect-dark-pal = (pkgs.callPackage ./pkgs/perfect-dark { }).pal;
-  perfect-dark-jpn = (pkgs.callPackage ./pkgs/perfect-dark { }).jpn;
+  suyu = (pkgs.callPackage ./pkgs/suyu { }).suyu;
 }
