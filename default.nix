25ec457c763d9b757d7be222e243bd04c7c9b013
mitsuru <codeberg@yamamech.sh>
2024-08-03 18:58:21 +0100

    Added Viper and rose-pine-sddm packages

--- default.nix
+++ default.nix
@@ -4,15 +4,16 @@
   # lib = import ./lib { inherit pkgs; }; # functions
   # modules = import ./modules; # NixOS modules
   # overlays = import ./overlays; # nixpkgs overlays
-  flight-core = pkgs.callPackage ./pkgs/flight-core { };
-  macos-cursors = pkgs.callPackage ./pkgs/macOS-cursors { };
-  tokyo-night-gtk-icons = pkgs.callPackage ./pkgs/tokyo-night-gtk-icons { };
-  tokyo-night-sddm = pkgs.callPackage ./pkgs/tokyo-night-sddm { };
-
   citra-canary = (pkgs.callPackage ./pkgs/citra { }).canary;
   citra-nightly = (pkgs.callPackage ./pkgs/citra { }).nightly;
+  flight-core = pkgs.callPackage ./pkgs/flight-core { };
+  macos-cursors = pkgs.callPackage ./pkgs/macOS-cursors { };
   perfect-dark-jpn = (pkgs.callPackage ./pkgs/perfect-dark { }).jpn;
   perfect-dark-ntsc = (pkgs.callPackage ./pkgs/perfect-dark { }).ntsc;
   perfect-dark-pal = (pkgs.callPackage ./pkgs/perfect-dark { }).pal;
+  rose-pine-sddm = pkgs.callPackage ./pkgs/rose-pine-sddm { };
   suyu = (pkgs.callPackage ./pkgs/suyu { }).suyu;
+  tokyo-night-gtk-icons = pkgs.callPackage ./pkgs/tokyo-night-gtk-icons { };
+  tokyo-night-sddm = pkgs.callPackage ./pkgs/tokyo-night-sddm { };
+  viper = pkgs.callPackage ./pkgs/viper { };
 }
