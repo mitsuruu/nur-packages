2aec190b56f31d352fc9218a97fb832c67e694b7
mitsuru <codeberg@yamamech.sh>
2025-03-05 13:17:28 +0000

    revert: add linux_xanmod_edge kernel package
    This reverts commit 2df31dc968cd3644d5993107759108b9c528c61d.

--- flake.nix
+++ flake.nix
@@ -6,7 +6,16 @@
   };
   outputs = { self, nixpkgs, git-hooks }:
     let
-      forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
+      systems = [
+        "x86_64-linux"
+        "i686-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+        "aarch64-linux"
+        "armv6l-linux"
+        "armv7l-linux"
+      ];
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
     in
     {
       checks = forAllSystems (system: {
