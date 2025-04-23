854c6b3b2435b1853b7b85ec3a25adf9f836d28f
mitsuru <codeberg@yamamech.sh>
2025-04-23 22:30:35 +0100

    flightcore: 2.23.1 -> 3.0.4

--- pkgs/flight-core/default.nix
+++ pkgs/flight-core/default.nix
@@ -1,63 +1,92 @@
-{
-  appimageTools,
-  lib,
-  fetchurl,
-  libthai,
-  harfbuzz,
-  fontconfig,
-  freetype,
-  libz,
-  libX11,
-  mesa,
-  libdrm,
-  fribidi,
-  libxcb,
-  libgpg-error,
-  libGL,
-}: let
-  pname = "flight-core";
-  version = "2.23.1";
+{ cargo-tauri
+, dbus
+, freetype
+, gsettings-desktop-schemas
+, libsoup_2_4
+, openssl
+, pkg-config
+, rustPlatform
+, webkitgtk_4_1
+, wrapGAppsHook3
+, buildNpmPackage
+, fetchFromGitHub
+,
+}:
 
-  src = fetchurl {
-    url = "https://github.com/R2NorthstarTools/FlightCore/releases/download/v${version}/${pname}_${version}_amd64.AppImage";
-    hash = "sha256-fXq/biSGnTbJ3mr19kJNPzYIy/wLyyXlMhsdVLDjMQQ=";
-    name = "${pname}-${version}.AppImage";
+let
+  pname = "FlightCore";
+  version = "3.0.4";
+
+  cargoHash = "sha256-JFpT+cqnFbkmPzYUygTEbuTg7LwTx2gR86i7FzAH54E=";
+  npmDepsHash = "sha256-DzPuvgmpaPTUUbrSnAVbll5mqZRPAWqHGdmNtgtLtU4=";
+
+  src = fetchFromGitHub {
+    owner = "R2NorthstarTools";
+    repo = "FlightCore";
+    rev = "v${version}";
+    hash = "sha256-rzBaZEnIaqxfyusVNTWU0qKggDnI1tSR7WN8HE6epWo=";
   };
 
-  appimageContents = appimageTools.extractType2 {
-    inherit pname version src;
-  };
+  frontend-build = buildNpmPackage {
+    pname = "flightcore-ui";
+    inherit version src npmDepsHash;
+    sourceRoot = "${src.name}/src-vue";
 
-  libs = [
-    libthai
-    harfbuzz
-    fontconfig
-    freetype
-    libz
-    libX11
-    mesa
-    libdrm
-    fribidi
-    libxcb
-    libgpg-error
-    libGL
-  ];
-in
-  appimageTools.wrapType2 {
-    inherit pname version src;
-    multiPkgs = null; # no 32bit needed
-    extraPkgs = p: (appimageTools.defaultFhsEnvArgs.multiPkgs p) ++ libs;
-    extraInstallCommands = ''
-      install -m 444 -D ${appimageContents}/${pname}.desktop -t $out/share/applications
-      cp -r ${appimageContents}/usr/share/icons $out/share
+    installPhase = ''
+      runHook preInstall
+
+      mkdir -p $out/dist
+      cp -r dist/** $out/dist
+
+      runHook postInstall
     '';
+  };
+in
+rustPlatform.buildRustPackage {
+  inherit version pname src cargoHash;
+  sourceRoot = "${src.name}/src-tauri";
 
-    meta = {
-      description = "Installer/Updater/Launcher for Northstar";
-      homepage = "https://github.com/R2NorthstarTools/FlightCore";
-      license = lib.licenses.mit;
-      mainProgram = "flight-core";
-      maintainers = with lib.maintainers; [NotAShelf];
-      platforms = ["x86_64-linux"];
-    };
-  }
+  preConfigure = ''
+    mkdir -p dist
+    cp -R ${frontend-build}/dist/** dist
+  '';
+
+  # patches = [ ./remove-windows-requirement.patch ];
+
+  # copy the frontend static resources to final build directory
+  # Also modify tauri.conf.json so that it expects the resources at the new location
+  postPatch = ''
+    substituteInPlace ./tauri.conf.json \
+      --replace-fail '"frontendDist": "../src-vue/dist"' '"frontendDist": "dist"' \
+      --replace-fail '"beforeBuildCommand": "cd src-vue && npm run build",' '"beforeBuildCommand": "",' \
+      --replace-fail '"createUpdaterArtifacts": "v1Compatible",' '"createUpdaterArtifacts": false,'
+  '';
+
+  nativeBuildInputs = [
+    cargo-tauri.hook
+    pkg-config
+    wrapGAppsHook3
+  ];
+
+  buildInputs = [
+    dbus
+    openssl
+    freetype
+    libsoup_2_4
+    webkitgtk_4_1
+    gsettings-desktop-schemas
+  ];
+
+  # WEBKIT_DISABLE_COMPOSITING_MODE essential in NVIDIA + compositor https://github.com/NixOS/nixpkgs/issues/212064#issuecomment-1400202079
+  # postFixup = ''
+  #   wrapProgram "$out/bin/gmm" \
+  #     --set WEBKIT_DISABLE_COMPOSITING_MODE 1
+  # '';
+
+  meta = {
+    description = "Installer/Updater/Launcher for Northstar";
+    homepage = "https://github.com/R2NorthstarTools/FlightCore";
+    platforms = [ "x86_64-linux" ];
+    # mainProgram = "gmm";
+  };
+}
