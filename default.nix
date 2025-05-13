dd431859ac19eb5471e4de17fd3ff0f0014d62b3
mitsuru <codeberg@yamamech.sh>
2025-05-13 19:58:24 +0100

    init: sunshine-git@2ca2ea4

--- default.nix
+++ default.nix
@@ -14,6 +14,7 @@
   perfect-dark-pal = (pkgs.callPackage ./pkgs/perfect-dark { }).pal;
   pixel-perfect-svg = pkgs.callPackage ./pkgs/pixel-perfect-svg { };
   rose-pine-sddm = pkgs.callPackage ./pkgs/rose-pine-sddm { };
+  sunshine-git = pkgs.callPackage ./pkgs/sunshine-git { };
   tokyo-night-gtk-icons = pkgs.callPackage ./pkgs/tokyo-night-gtk-icons { };
   tokyo-night-sddm = pkgs.callPackage ./pkgs/tokyo-night-sddm { };
   totk-optimizer = pkgs.callPackage ./pkgs/totk-optimizer { };
