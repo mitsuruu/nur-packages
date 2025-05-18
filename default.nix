248974576da0bb49c20c25172ee9062eef800d20
mitsuru <codeberg@yamamech.sh>
2025-05-18 17:07:46 +0100

    drop: macos-cursors

--- default.nix
+++ default.nix
@@ -9,7 +9,6 @@
   eden = pkgs.callPackage ./pkgs/eden { };
   flight-core = pkgs.callPackage ./pkgs/flight-core { };
   gmm = pkgs.callPackage ./pkgs/gmm { };
-  macos-cursors = pkgs.callPackage ./pkgs/macOS-cursors { };
   pixel-perfect-svg = pkgs.callPackage ./pkgs/pixel-perfect-svg { };
   sunshine-git = pkgs.callPackage ./pkgs/sunshine-git { };
   totk-optimizer = pkgs.callPackage ./pkgs/totk-optimizer { };
